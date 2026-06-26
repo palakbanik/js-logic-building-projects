7d6f1a80fd233e119eab6adec304a6081c279a31
palakbanik <polokbanik7@gmail.com>
2026-06-27 00:43:45 +0600

    problem alphabetic order check

--- problem-solve/23_alphabetical_order.js
+++ problem-solve/23_alphabetical_order.js
@@ -1,9 +1,17 @@
-const alphabeticOrder = (str) => {
-    console.log(str);
+function isAlphabeticOrder(str) {
+    str = str.toLowerCase().replace(/ /g, "");
 
-    for (let i = 0; i < str.length; i++) {
-        
+    for (let i = 0; i < str.length - 1; i++) {
+        if (str[i] > str[i + 1]) {
+            return false;
+        }
     }
-};
 
-console.log(alphabeticOrder("abcd"));
+    return true;
+}
+
+console.log("abcd", isAlphabeticOrder("abcd"));
+console.log("abac", isAlphabeticOrder("abac"));
+console.log("acimn", isAlphabeticOrder("acimn"));
+console.log("aAcim n", isAlphabeticOrder("aAcim n"));
+console.log("acimlm", isAlphabeticOrder("acimlm"));
